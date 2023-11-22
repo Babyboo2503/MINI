0bbb05b81631b439418ed0796653ceda259ea82d
Luong Minh The Vinh <92390358+lmtvinh@users.noreply.github.com>
2023-11-22 10:17:50 +0700

    .
    .

---
+++ MINI/GUI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINI.GUI
+{
+    internal class Class1
+    {
+    }
+}
