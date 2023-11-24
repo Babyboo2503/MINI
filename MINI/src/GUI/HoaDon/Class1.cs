4d32f12273d40a9b1a222deda29d86fa8b5869a1
Luong Minh The Vinh <92390358+lmtvinh@users.noreply.github.com>
2023-11-24 10:00:51 +0700

    .
    .

---
+++ MINI/src/GUI/HoaDon/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINI.src.GUI.HoaDon
+{
+    internal class Class1
+    {
+    }
+}
---
+++ MINI/src/GUI/TaiKhoan/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINI.src.GUI.TaiKhoan
+{
+    internal class Class1
+    {
+    }
+}
