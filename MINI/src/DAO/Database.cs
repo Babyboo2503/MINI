19d926fe9b5f87c02952ed212fcb73553f27d09a
Luong Minh The Vinh <92390358+lmtvinh@users.noreply.github.com>
2023-11-29 15:51:07 +0700

    DAO Database
    DAO Database

---
+++ MINI/src/DAO/Database.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINI.src.DAO
+{
+    public class Database
+    {
+        SqlConnection sqlConn;
+        SqlDataAdapter da;
+        DataSet ds;
+        public Database()
+        {
+            string strCnn = "Data Source=.; Database=QLthuvien;Integrated Security = True"; 
+            sqlConn = new SqlConnection(strCnn);
+        }
+        public DataTable Execute(string sqlStr)
+        {
+            da = new SqlDataAdapter(sqlStr, sqlConn); ds = new DataSet();
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+        public void ExecuteNonQuery(string strSQL)
+        {
+            SqlCommand sqlcmd = new SqlCommand(strSQL, sqlConn); sqlConn.Open();
+            sqlcmd.ExecuteNonQuery();
+            sqlConn.Close();
+        }
+
+    }
+}
