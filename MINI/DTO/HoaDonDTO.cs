890c75f87ab768e2683280d6c4dba70e9ecfc1c9
LAD2k3 <151028886+LAD2k3@users.noreply.github.com>
2023-12-04 00:09:21 +0700

    Add files via upload

--- MINI/DTO/HoaDonDTO.cs
+++ MINI/DTO/HoaDonDTO.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace MINI.DTO
+{
+    internal class HoaDonDTO
+    {
+        public int idHoaDon {  get; set; }
+        public int idNhanVien { get; set; }
+        public int idKhachHang { get; set; }
+        public DateTime ngayLap {  get; set; }
+        public int idKhuyenMai { get; set; }
+        public decimal tongHoaDon { get; set; }
+    }
+    
+
+}
