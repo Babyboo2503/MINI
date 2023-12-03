2c35a283fdf0d8195d64ecb54821e167a5c63590
LAD2k3 <151028886+LAD2k3@users.noreply.github.com>
2023-12-04 00:07:42 +0700

    Add files via upload

--- MINI/GUI/BanHang/BanHang.cs
+++ MINI/GUI/BanHang/BanHang.cs
@@ -1,1 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace MINI.GUI.BanHang
+{
+    public partial class BanHang : Form
+    {
+        public BanHang()
+        {
+            InitializeComponent();
+        }
+
+        private void BanHang_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'miniMarketDataSet1.KhuyenMai' table. You can move, or remove it, as needed.
+            this.khuyenMaiTableAdapter.Fill(this.miniMarketDataSet1.KhuyenMai);
+            // TODO: This line of code loads data into the 'miniMarketDataSet1.KhachHang' table. You can move, or remove it, as needed.
+            this.khachHangTableAdapter.Fill(this.miniMarketDataSet1.KhachHang);
+            // TODO: This line of code loads data into the 'miniMarketDataSet1.NhanVien' table. You can move, or remove it, as needed.
+            this.nhanVienTableAdapter.Fill(this.miniMarketDataSet1.NhanVien);
+
+        }
+    }
+}
