8568da113c51238aaae04ddd9aa80125e664a5c6
BabyBoo <118751691+Babyboo2503@users.noreply.github.com>
2023-12-12 02:31:50 +0700

    Update Database trong DAO

--- MINI/src/DAO/Database.cs
+++ MINI/src/DAO/Database.cs
@@ -1,38 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MINI.src.DAO
 {
-    public class Database
+    internal class Database
     {
-        SqlConnection sqlConn;
-        SqlDataAdapter da;
-        DataSet ds;
+        SqlConnection sqlConn; //Doi tuong ket noi CSDL
+        SqlDataAdapter da;//Bo dieu phoi du lieu
+        DataSet ds; //Doi tuong chhua CSDL khi giao tiep
         public Database()
         {
-            string strCnn = "Data Source= localhost; Database=MiniMarket;Integrated Security = True";
+            string strCnn = "Data Source=LAPTOP-1OI04F38;Initial Catalog=MiniMarket;Integrated Security=True";
             sqlConn = new SqlConnection(strCnn);
         }
-
+        //Phuong thuc de thuc hien cau lenh strSQL truy vân du lieu
         public DataTable Execute(string sqlStr)
         {
-            da = new SqlDataAdapter(sqlStr, sqlConn);
-            ds = new DataSet();
+            da = new SqlDataAdapter(sqlStr, sqlConn); ds = new DataSet();
             da.Fill(ds);
             return ds.Tables[0];
         }
 
-        public void ExecuteNonQuery(string strSQL)
+        //Phuong thuc de thuc hien cac lenh Them, Xoa, Sua
+        public int ExecuteNonQuery(string strSQL)
         {
             SqlCommand sqlcmd = new SqlCommand(strSQL, sqlConn);
-            sqlConn.Open();
-            sqlcmd.ExecuteNonQuery();
+            sqlConn.Open(); //Mo ket noi
+            int row = sqlcmd.ExecuteNonQuery();//Lenh hien lenh Them/Xoa/Sua
+            sqlConn.Close();//Dong ket noi
+            return row;
+        }
+
+        public int ExecuteReader(string strSQL)
+        {
+            SqlCommand sqlcmd = new SqlCommand(strSQL, sqlConn);
+            sqlConn.Open(); //Mo ket noi
+            SqlDataReader reader = sqlcmd.ExecuteReader();
+            int row = 0;
+            while (reader.Read()) ++row;
             sqlConn.Close();
+            return row;
         }
     }
 }
